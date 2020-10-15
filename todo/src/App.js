a9eb577fcfea60cb69bded00d80a4e3d058a7954
jimmychen10 <jimmychen1996@yahoo.com>
2020-10-15 01:18:45 -0700

    Added states and event handlers

--- todo/src/App.js
+++ todo/src/App.js
@@ -9,20 +9,41 @@ class App extends Component {
   constructor() {
       super()
       this.state = {
-          todos: todosData
+          items: todosData
       }
-  }
-  
-  render() {
-      const todoItems = this.state.todos.map(item => <Item key={item.id} item={item}/>)
-      
-      return (
-          <div className="todo-list">
-              {todoItems}
-          </div>
-      )    
-  }
+      this.handleChange = this.handleChange.bind(this)
+    }
+    
+    handleChange(id) {
+        // Update state so that the item with the given id flips `completed` from false to true (or vise versa)
+        // Remember not to modify prevState directly, but instead to return a new version of state with the change you want included in that update. (Think how you might use the `.map` method to do this)
+        this.setState(prevState =>{
+            const updatedItems = prevState.items.map(item =>{
+                if(item.id === id){
+                    item.completed = !item.completed
+                }
+                return item
+            }
+            
+            )
+            return{
+                items: updatedItems
+            }
+        }
+
+        )
+    
+    }
+    
+    render() {
+        const todoItems = this.state.items.map(item => <Item key={item.id} item={item} fun = {this.handleChange}/>)
+        
+        return (
+            <div className="todo-list">
+                {todoItems}
+            </div>
+        )    
+    }
 }
 
 export default App
-
